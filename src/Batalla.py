d32cca8225923fba6e4c3f3609d0aecb9ce6b428
MCH <matias.david.chavez@gmail.com>
2018-09-12 21:08:24 -0300

    Agregado de Batalla.

---
+++ src/Batalla.py
@@ -0,0 +1,57 @@
+# Variables para iniciar-restaurar una batalla.
+
+# Monstruo 1 [nombre, vida, elementos, cantAtaquesEsp, turno] 
+# Monstruo 2 [nombre, vida, elementos, cantAtaquesEsp, turno]
+
+class Batalla:
+	
+	def __init__(self)
+		Jugadores = []
+		
+	def mostrarTitulo():
+		os.system('cls' if os.name == 'nt' else 'clear')
+
+		print("\t**********************************************")
+		print("\t***  Batalla de Monstruos  ***")
+		print("\t**********************************************")
+	
+	def mostrarTurno():
+		print("\t***  Es el turno de [" + getTurno() + "] *****")
+		print("\t**********************************************")
+		
+	def configJugador():
+		#Pedir nombre jugador 1
+		#Pedir elemento 1
+		#Pedir elemento 2
+		#definimos vida
+		#ataques restantes
+		#Pedir nombre jugador 2
+		#Pedir elemento 1
+		#Pedir elemento 2
+		pass
+	
+	def getTurno():
+		Jugador = for x in Jugadores if x.Turno()
+		return Jugador.nombre()
+		
+	def comenzarPelea():
+		pass
+		
+	def cargarJuego():
+		pass
+		
+	def getNombreGanador() {
+		return this.defensor.getNombre();
+	}
+
+
+
+	
+
+	
+
+	
+
+	
+
+}
